01db375e0145cb01014dd9863d16d2e334f4bf21
Hoa Trinh <tqhoa.imi@gmail.com>
2019-05-30 21:41:57 +0700

    test

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 //Book
 class Book {
   constructor(title, author, pages, isRead) {
@@ -18,10 +16,10 @@ function addBook(books) {
   let title = document.querySelector('input[name="title"]').value;
   let author = document.querySelector('input[name="author"]').value;
   let pages = document.querySelector('input[name="pages"]').value;
-  let readstatus = document.getElementByName('read');
+  let readRadio = document.getElementByName('read');
   let isRead = false;
 
-  readstatus.forEach((radio) => if (radio.checked) {isRead = radio.value});
+  readRadio.forEach(function(radio) {if (radio.checked) {isRead = radio.value}});
 
   let newBook = new Book(title, author, pages, isRead);
 
@@ -33,16 +31,15 @@ function render(books) {
 
     books.forEach(function(book) {
     let div = document.createElement('div');
-    let readStatus() = (book.isRead) ? 'Read' : 'Not read';
+    let readStatus = (book.isRead) ? 'Read' : 'Not read';
     div.className = "flex-1";
-
     div.innerHTML = `<div class="w-min-sm bg-white m-3 rounded shadow-lg overflow-hidden">
         <img src="https://picsum.photos/1000/600">
           <div id="title" class="font-bold text-xl m-1 py-2">${book.title}</div>
           <div id="author" class="font-bold text-xl m-1 py-2">${book.author}</div>
           <div id="pages" class="font-bold text-xl m-1 py-2">${book.pages}</div>
           <div class="flex justify-around mb-4">
-            <input type="button" id="isread"  value="readStatus"
+            <input type="button" id="isread"  value="${readStatus}"
             class="button text-white bg-green-600 m-1 p-2 hover:bg-green-500"/>
             <input type="button" id="del" value="Del"
             class="button text-white bg-red-400 m-1 p-2 hover:bg-red-300"/>
@@ -50,14 +47,18 @@ function render(books) {
       </div>`;
 
     library.appendChild(div);
-  })
+  });
 }
-let addBook = document.getElementById('addBook');
-addBook.addEventListener("click", function() {
+
+
+addBookBtn.addEventListener("click", function() {
     addBook(books);
     render(books);
-};
+});
+
 let books = [];
 let book1 = new Book('a', 'b', 2, true);
+
 books=[book1];
+
 render(books);
